19edec00041042004ef1d7e053316e5ad0117067
Enngage <ennspassion@gmail.com>
2020-08-18 16:15:50 +0200

    feat: simplifies code in components

--- src/kentico/kenticoConfig.js
+++ src/kentico/kenticoConfig.js
@@ -6,11 +6,11 @@ define({
   // courseId: 'dev05-12', // MVC Developer - Identity
   
   // ===== Courses for business users =====
-  courseId: "dev03-12", // MVC Developer - Essentials
+  courseId: "dev-12-mvc-essentials", // MVC Developer - Essentials
   // courseId: 'business01-12', // Content management
   // courseId: 'business02-12', // Online marketing
   // courseId: 'business03-12', // Personalization
-  language: 'en', // Language of course (only en supported right now)
+  language: 'default', // Language of course
 
   // serverUrl: 'https://kentico-adapt-live.azurewebsites.net',
   // serverUrl: 'https://richardsadaptmvc.azurewebsites.net',
