32d8377383630f21dc4b76fdac5a4d750f4fd73d
lukas-xb <lukas@lukasbajer.cz>
2020-09-07 14:44:13 +0200

    Adds Xperience styling

--- src/kentico/kenticoConfig.js
+++ src/kentico/kenticoConfig.js
@@ -4,24 +4,27 @@ define({
   // ===== Courses for developers =====
   // courseId: 'dev04-12', // MVC Developer - Builders
   // courseId: 'dev05-12', // MVC Developer - Identity
-  
+
   // ===== Courses for business users =====
-  courseId: "dev-12-mvc-essentials", // MVC Developer - Essentials
+  // courseId: "dev-12-mvc-essentials", // MVC Developer - Essentials
   // courseId: 'business01-12', // Content management
   // courseId: 'business02-12', // Online marketing
-  // courseId: 'business03-12', // Personalization
-  language: 'default', // Language of course
+  courseId: "business03-12", // Personalization
+  // TODO: NEW CM
+  // language: 'default', // Language of course
+  //TODO: Old CM
+  language: "en", // Language of course
 
-  // serverUrl: 'https://kentico-adapt-live.azurewebsites.net',
+  serverUrl: "https://kentico-adapt-live.azurewebsites.net",
   // serverUrl: 'https://richardsadaptmvc.azurewebsites.net',
-  serverUrl: 'http://localhost:51355',
-  coursesPath: 'courses',
+  // serverUrl: "http://localhost:51355",
+  coursesPath: "courses",
 
   files: {
-    articles: 'articles.json',
-    blocks: 'blocks.json',
-    components: 'components.json',
-    contentObjects: 'contentObjects.json',
-    course: 'course.json'
-  }
+    articles: "articles.json",
+    blocks: "blocks.json",
+    components: "components.json",
+    contentObjects: "contentObjects.json",
+    course: "course.json",
+  },
 });
